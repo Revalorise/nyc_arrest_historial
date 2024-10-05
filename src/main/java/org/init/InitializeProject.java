85a694218a27e058e59a0336a31624fe4d267317
Bon <Blackball007b@gmail.com>
2024-10-05 12:51:34 +0700

    partially modified InitializeProject.java file

--- src/main/java/org/init/InitializeProject.java
+++ src/main/java/org/init/InitializeProject.java
@@ -3,10 +3,10 @@ package org.init;
 import org.helper_utility.BucketUtil;
 import org.helper_utility.ZipSerialize;
 
-import java.io.IOException;
+import java.util.ArrayList;
 
 final class InitializeProject {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         ZipSerialize serializer = new ZipSerialize();
         BucketUtil bucketUtil =
                 new BucketUtil("gcp-data-engineering-426405");
@@ -15,6 +15,22 @@ final class InitializeProject {
         String outputFilePath
                 = "C:\\Users\\Ball\\Desktop\\Weather_pipeline\\NYPD_Arrests_Data_Historic.zip";
 
+        String bucketName = "gcp-data-engineering-426405-nyc-crime-historic";
+        String objectName = "NYPD_Arrests_Data_Historic.zip";
+
         serializer.serializeToZIP(filePath, outputFilePath);
+        bucketUtil.createBucket(bucketName);
+
+        assert bucketUtil.checkIfBucketExists(
+                "gcp-data-engineering-426405-nyc-crime-historic"
+        );
+
+        bucketUtil.uploadObject(bucketName, objectName, outputFilePath);
+
+        ArrayList<String> objectList = bucketUtil.listBucketObjects(bucketName);
+        assert !objectList.isEmpty();
+        assert objectList.contains(objectName);
+
+
     }
 }
