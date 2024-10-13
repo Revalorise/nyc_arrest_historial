3572eba0bc8d753ccc9852002d6fb4eccfb2e1d0
Bon <Blackball007b@gmail.com>
2024-10-13 08:49:39 +0700

    minor change

--- src/main/java/org/main/PGDatabase.java
+++ src/main/java/org/main/PGDatabase.java
@@ -43,7 +43,7 @@ public class PGDatabase {
         }
     }
 
-    public static boolean tableIsEmpty(String postgresURL, String username, String password, String tableName) {
+    private static boolean tableIsEmpty(String postgresURL, String username, String password, String tableName) {
         boolean flag;
         try {
             conn = DatabaseUtil.getConnection(postgresURL, username, password);
@@ -68,25 +68,28 @@ public class PGDatabase {
 
     public static void insertNYCArrestData(String postgresURL, String username, String password) {
         try {
-            conn = DatabaseUtil.getConnection(postgresURL, username, password);
+            if (tableIsEmpty(postgresURL, username, password, "nypd_arrest_data_historic")) {
+                conn = DatabaseUtil.getConnection(postgresURL, username, password);
 
-            String insertCsvData = "COPY nypd_arrest_data_historic"
-                    + "(arrest_key, arrest_date, arrest_boro, age_group, perp_sex, perp_race) "
-                    + "FROM 'C:\\Users\\Ball\\Desktop\\Weather_pipeline\\temp\\Transformed_NYPD_Arrests_Data_Historic.csv' "
-                    + "DELIMITER ',' "
-                    + "CSV HEADER ";
+                String insertCsvData = "COPY nypd_arrest_data_historic"
+                        + "(arrest_key, arrest_date, arrest_boro, age_group, perp_sex, perp_race) "
+                        + "FROM 'C:\\Users\\Ball\\Desktop\\Weather_pipeline\\temp\\Transformed_NYPD_Arrests_Data_Historic.csv' "
+                        + "DELIMITER ',' "
+                        + "CSV HEADER ";
 
-            Statement statement = null;
-            try {
-                statement = conn.createStatement();
-                statement.executeUpdate(insertCsvData);
-            } finally {
-                DatabaseUtil.closeStatement(statement);
+                Statement statement = null;
+                try {
+                    statement = conn.createStatement();
+                    statement.executeUpdate(insertCsvData);
+                } finally {
+                    DatabaseUtil.closeStatement(statement);
+                }
+
+                DatabaseUtil.commit(conn);
+                System.out.println("insertCsvData statement executed.");
+            } else {
+                System.out.println("Table already exists or isn't empty.");
             }
-
-            DatabaseUtil.commit(conn);
-            System.out.println("insertCsvData statement executed.");
-
         } catch (SQLException | ClassNotFoundException e) {
             System.out.println(e.getMessage());
             DatabaseUtil.rollback(conn);
